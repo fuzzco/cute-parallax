544043ccf6c6c53f975817c181e054a40e7fc46f
Sander Moolin <sandermoolin@gmail.com>
2019-05-10 13:45:55 -0400

    WIP on turning into class

--- src/index.js
+++ src/index.js
@@ -1,24 +1,40 @@
 let nodes = []
+let rate = 0.09
 
-function parallaxAnimation() {
-    // get number of pixels document has scrolled vertically
-    let scrolltop = window.pageYOffset
-    nodes.forEach(node => {
-        // move at 9% of scroll rate
-        node.style.transform = `translate3d(0, ${-scrolltop * rate}px, 0)`
-    })
-}
+export default class {
+    constructor(selector = `.js-parallax`, rate = 0.09, start = true) {
+        this.selector = selector
+        this.nodes = Array(...document.querySelectorAll(this.selector))
+        this.rate = rate
 
-function runParallax() {
-    // on page scroll
-    requestAnimationFrame(parallaxAnimation) // call parallaxAnimation() on next available screen paint
-}
+        if (start) {
+            this.start()
+        }
+    }
 
-const clearParallax = () => {
-    window.removeEventListener('scroll', runParallax)
-}
+    start() {
+        window.addEventListener('scroll', this.parallaxAnimation)
+    }
+    clear() {
+        this.nodes.forEach(node => {
+            node.style.transform = ''
+        })
+    }
+    stop(clear = true) {
+        window.removeEventListener('scroll', this.parallaxAnimation)
+        if (clear) {
+            this.clear()
+        }
+    }
 
-export default (selector = `.js-parallax`, rate = 0.09) => {
-    nodes = Array(...document.querySelectorAll(selector))
-    window.addEventListener('scroll', runParallax)
+    parallaxAnimation() {
+        requestAnimationFrame(() => {
+            // get number of pixels document has scrolled vertically
+            let scrolltop = window.pageYOffset
+            this.nodes.forEach(node => {
+                node.style.transform = `translate3d(0, ${-scrolltop *
+                    rate}px, 0)`
+            })
+        })
+    }
 }
